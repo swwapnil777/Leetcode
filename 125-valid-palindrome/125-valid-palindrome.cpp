dc17cc630cad7b91a4c5da4e65b36249fceb5544
swwapnil777 <108779988+swwapnil777@users.noreply.github.com>
2022-08-15 23:39:12 +0530

    Time: 8 ms (58.84%), Space: 7.7 MB (24.57%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,24 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    bool Helper(string &str,int i,int n){
-        if(i==n-1){
+    bool Helper(string &str, int i, int n)
+    {
+        if (i == n - 1)
+        {
             return true;
         }
-        return str[i]==str[n-i-1] && Helper(str,i+1,n);
+        return str[i] == str[n - i - 1] && Helper(str, i + 1, n);
     }
-    bool isPalindrome(string s) {
+    bool isPalindrome(string s)
+    {
         string str;
-        for(int i=0;i<s.size();++i){
-           if(s[i]>='A' && s[i]<='Z' || s[i]>='a' && s[i]<='z'|| s[i]>='0' && s[i]<='9'){
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9')
+            {
                 str.push_back(s[i]);
             }
-           }
-        
-     transform(str.begin(),str.end(),str.begin(),::tolower);
-        int i=0;
+        }
+
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+        int i = 0;
         int n = str.size();
-        if(str.size()==0) return true;
-        return Helper(str,i,n)==true?true:false;
-        
+        if (str.size() == 0)
+            return true;
+        return Helper(str, i, n) == true ? true : false;
     }
 };
