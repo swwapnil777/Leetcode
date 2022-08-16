35272e3eb54d40e48d464cb93c91894174bce19a
swwapnil777 <108779988+swwapnil777@users.noreply.github.com>
2022-08-16 18:05:05 +0530

    Time: 4 ms (29.29%), Space: 6.1 MB (81.83%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(auto &x:s){
-            if(x>=65 && x<=90){
-                x=x+32;
-            }
+            if(x>=65 && x<=90)
+                x=x+32;   
         }
         return s;
     }
