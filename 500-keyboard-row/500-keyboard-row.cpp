81425a629c499e67c33f33cca1a13b4fd4c3e3ce
swwapnil777 <108779988+swwapnil777@users.noreply.github.com>
2022-08-19 23:44:08 +0530

    Time: 2 ms (51.99%), Space: 6.6 MB (99.92%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.cpp
+++ 500-keyboard-row/500-keyboard-row.cpp
@@ -1,43 +1,44 @@
 class Solution {
-public:
-      unordered_set<char> set1= {'q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p','Q','W','E','R','T','Y','U','I','P','O'};
-      unordered_set<char> set2= {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ,'A','S','D','F','G','H','J','K','L'}; 
-      unordered_set<char> set3= { 'z', 'x', 'c', 'v', 'b' ,'n', 'm','Z','X','C','V','B','N','M'};   
-    bool helper(string &temp){
-            char ch = temp[0];
-            if(set1.find(ch)!=set1.end()){
-                for(int i=1;i<temp.size(); ++i){
-                    if(set1.find(temp[i])==set1.end()){
-                        return false;
-                    }
-                   
-                }
+    public:
+        string s1 = "qwertyuiopQWERTYUIOP";
+        string s2 = "asdfghjklASDFGHJKL";
+        string s3 = "zxcvbnmZXCVBNM";
+        bool valid(string &s){
+        char ch = s[0];
+            size_t f;
+            f = s1.find(ch);
+        if(f!=string::npos){
+            for(int i = 1;i<s.length();i++){
+                f = s1.find(s[i]);
+                if(f==string::npos)
+                    return 0;
             }
-           else if(set2.find(ch)!=set2.end()){
-                for(int i=1;i<temp.size(); ++i){
-                    if(set2.find(temp[i])==set2.end()){
-                        return false;
-                    }    
-                }
+        }
+            f = s2.find(ch);
+        
+        if(f!=string::npos){
+            for(int i = 1;i<s.length();i++){
+                f = s2.find(s[i]);
+                if(f==string::npos)
+                    return 0;
             }
-           else if(set3.find(ch)!=set3.end()){
-                for(int i=1;i<temp.size(); ++i){
-                    if(set3.find(temp[i])==set3.end()){
-                        return false;
-                    }
-                    
-                }
+        }
+            f = s3.find(ch);
+        if(f!=string::npos){
+            for(int i = 1;i<s.length();i++){
+                f = s3.find(s[i]);
+                if(f==string::npos )
+                    return 0;
             }
-         return true;
-       
+        }
+        return 1;
     }
     vector<string> findWords(vector<string>& words) {
         vector<string> ans;
-        for(auto &x:words){
-            if(helper(x)) ans.push_back(x);
+        for(auto& word : words) {
+            if(valid(word))
+                ans.push_back(word);
         }
         return ans;
-    
-        
     }
 };
