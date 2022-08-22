9ff836a2bc8f1e306c9e000446573a13017ed731
swwapnil777 <108779988+swwapnil777@users.noreply.github.com>
2022-08-22 17:56:27 +0530

    Time: 4 ms (41.09%), Space: 6.3 MB (8.38%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool digitCount(string num) {
+        int n  = num.size()-1;
         unordered_map<int,int> map;
         for(int i=0;i<num.size();++i){
             int ch = num[i]-'0';
             map[ch]++;
         }
+        bool flag = true;
         for(int i=0;i<num.size();++i){
             int ch = num[i]-'0';
             if(map[i]!=ch){
-                return false;
+               flag =false;
             }
         }
-        return true;
+        if(flag==true) return true;
+        else{
+            return false;
+        }
         
     }
 };
