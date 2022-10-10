cee046bbb3e5e8a305ad1a749cb13e9622deeb10
swwapnil777 <108779988+swwapnil777@users.noreply.github.com>
2022-10-10 17:27:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,24 +4,15 @@ private:
     int dp[31];
 
 public:
-    Solution()
-    {
-        fill(dp, dp + 31, -1);
-    }
-    int solve(int n, int arr[])
-    {
-        if (n <= 1)
-            return n;
-        if (dp[n] != -1)
-            return dp[n];
-
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
-    }
     int fib(int n)
     {
-        int ans = solve(n, dp);
+        dp[0] = 0;
+        dp[1] = 1;
 
-        return ans;
+        for (int i = 2; i <= n; ++i)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
